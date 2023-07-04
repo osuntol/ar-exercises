791151dac4eea46aa3d2ca197f53ae086e492b23
Rotimi Osuntola Jr <rosuntola@gmail.com>
2023-07-04 22:55:52 +0000

    Exercise completed

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,8 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store1.employees.create(first_name: "Randy", last_name: "Golden", hourly_rate: 50)
+@store1.employees.create(first_name: "Floss", last_name: "Mark", hourly_rate: 70)
+@store1.employees.create(first_name: "Sandy", last_name: "Mocup", hourly_rate: 55)
+
--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -10,3 +10,14 @@ puts "Exercise 7"
 puts "----------"
 
 # Your code goes here ...
+puts "Store name"
+@store_name = gets.chomp
+
+@store = Store.new(name:@store_name)
+# binding.pry
+if @store.save
+@store.employees.create(first_name: "randy", last_name: "Virani", hourly_rate: 60)
+else 
+ puts "failed to save due to #{@store.errors.full_messages}"
+end 
+
